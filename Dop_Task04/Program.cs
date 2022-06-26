ba4929184a8c7b070cc1abfeb432b8e97aec8a64
yana sarvarova <serg.yana@mail.ru>
2022-06-27 00:50:01 +0300

    Дополнительная заача 4

---
+++ Dop_Task04/Program.cs
@@ -0,0 +1,9 @@
+﻿//Дано N секунд. Вывести время в формате часы:минуты:секунды
+Console.WriteLine("Введите количество секунд");
+int number = Convert.ToInt32(Console.ReadLine());
+int hour = number/60/60;
+int min = (number-(hour*60*60))/60;
+Console.Write($"{hour}:");
+Console.Write($"{min}:");
+Console.Write($"{number-(hour*60*60+min*60)}");
+Console.WriteLine();
