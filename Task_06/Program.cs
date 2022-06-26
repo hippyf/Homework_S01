dfac23836640324261411614531df2bf3cad4b19
yana sarvarova <serg.yana@mail.ru>
2022-06-26 13:22:29 +0300

    Задание 6

---
+++ Task_06/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт,
+является ли число чётным */
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0)
+Console.WriteLine($"{number} - чётное число");
+else Console.WriteLine($"{number} - нечётное число");
