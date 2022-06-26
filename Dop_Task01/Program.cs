50e2c5f9a972b6b5cd3017b919db3e67c76804e8
yana sarvarova <serg.yana@mail.ru>
2022-06-26 22:19:46 +0300

    Дополнительная задача 01

---
+++ Dop_Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пользователь вводит число N (N > 1). Программа должна вывести N единиц на экран.
+Console.WriteLine("Введите целое число больше 0");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if(number > 0)
+{
+    while(count <= number)
+    {
+        Console.Write("1, ");
+        count++;
+    }
+}
+else 
+{
+    Console.Write("Введено неверное число!");
+}
+Console.WriteLine();
