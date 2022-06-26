6a11fc17148854a6dd87469ceaf4c5ade1626f84
yana sarvarova <serg.yana@mail.ru>
2022-06-26 22:52:52 +0300

    Дополнительная задача 2

---
+++ Dop_Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Даны два числа a и b, a меньше b. 
+Вывести на экран сколько раз число a поместится в числе b. */
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число, оно должно быть больше первого");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (numberA < numberB) 
+{
+    result = result + numberB / numberA;
+    Console.WriteLine($"{result} - столько раз число {numberA} поместится в числе {numberB}");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверные числа!");
+}
