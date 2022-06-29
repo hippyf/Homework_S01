11a8f012a6c50ce2b78d9704f963d307e2b2a1ba
yana sarvarova <serg.yana@mail.ru>
2022-06-29 20:49:50 +0300

    Третий вариант решения задачи

---
+++ Task_08v3/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая принимает на вход число (N),
+а на выходе показывает все чётные числа от 1 до N
+Вариант1 */
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+for (int i = 1; i <= number; i++)
+{
+    if (i%2 ==0)
+    {
+        Console.Write(i+ " ");
+    }
+}
+Console.WriteLine();
